951823c51515695963fcbdb961d1bb92819f0eec
Joel Thiessen <jatjat@users.noreply.github.com>
2016-04-21 21:42:53 -0500

    convert AllTests to Kotlin

---
+++ src/test/kotlin/ca/joelathiessen/kaly2/tests/pc/AllTests.kt
@@ -0,0 +1,12 @@
+package ca.joelathiessen.kaly2.tests.pc
+
+import ca.joelathiessen.kaly2.tests.pc.unit.RobotTest
+import ca.joelathiessen.kaly2.tests.pc.unit.sensor.SpinnerTest
+import ca.joelathiessen.kaly2.tests.pc.unit.subconscious.SubconsciousTest
+import org.junit.runner.RunWith
+import org.junit.runners.Suite
+import org.junit.runners.Suite.SuiteClasses
+
+@RunWith(Suite::class)
+@SuiteClasses(SubconsciousTest::class, SpinnerTest::class, RobotTest::class)
+class AllTests
