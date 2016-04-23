06f3fdd8b260b425a12b3e7079cbe9f3644ac7c2
Joel Thiessen <jatjat@users.noreply.github.com>
2016-04-23 17:22:16 -0500

    Kotlinize SpinnerTest

---
+++ src/test/kotlin/ca/joelathiessen/kaly2/tests/pc/unit/sensor/SpinnerTest.kt
@@ -0,0 +1,75 @@
+package ca.joelathiessen.kaly2.tests.pc.unit.sensor
+
+import ca.joelathiessen.kaly2.subconscious.sensor.Spinner
+import lejos.robotics.RegulatedMotor
+import org.junit.Before
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.mockito.Mock
+import org.mockito.Mockito
+import org.mockito.runners.MockitoJUnitRunner
+
+import org.junit.Assert.*
+import org.mockito.Mockito.verify
+import org.mockito.Mockito.`when`
+
+@RunWith(MockitoJUnitRunner::class)
+class SpinnerTest {
+
+    @Mock
+    lateinit private var motor: RegulatedMotor
+
+    @Test
+    fun testSpin_DefaultLimits() {
+        val spinner = Spinner(motor)
+
+        `when`(motor.isMoving).thenReturn(false).thenReturn(true).thenReturn(false)
+
+        assertFalse(spinner.spinning())
+        assertTrue(spinner.turningClockwise())
+
+        spinner.spin()
+
+        assertTrue(spinner.spinning())
+        assertFalse(spinner.turningClockwise())
+        verify<RegulatedMotor>(motor).rotateTo(Spinner.DEFAULT_MAX_DETECTOR_ANGLE_DEG, true)
+
+        spinner.spin()
+
+        assertFalse(spinner.spinning())
+        assertTrue(spinner.turningClockwise())
+        verify<RegulatedMotor>(motor).rotateTo(Spinner.DEFAULT_MAX_DETECTOR_ANGLE_DEG, true)
+    }
+
+    @Test
+    fun testSpin_LargeLimits() {
+        val spinner = Spinner(motor, -360, 720)
+
+        `when`(motor.isMoving).thenReturn(false).thenReturn(true).thenReturn(false)
+
+        assertFalse(spinner.spinning())
+        assertTrue(spinner.turningClockwise())
+
+        spinner.spin()
+
+        assertTrue(spinner.spinning())
+        assertFalse(spinner.turningClockwise())
+        verify<RegulatedMotor>(motor).rotateTo(-360, true)
+
+        spinner.spin()
+
+        assertFalse(spinner.spinning())
+        assertTrue(spinner.turningClockwise())
+        verify<RegulatedMotor>(motor).rotateTo(720, true)
+    }
+
+    @Test
+    fun testGetAngle() {
+        val spinner = Spinner(motor)
+
+        `when`(motor.tachoCount).thenReturn(77)
+
+        assertEquals(spinner.angle.toDouble(), Math.toRadians(77.0), 0.0001)
+    }
+
+}
