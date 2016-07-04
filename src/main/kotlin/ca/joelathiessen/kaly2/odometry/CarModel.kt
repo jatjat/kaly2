c2f8276ed6b65de94a41e18b1d210537385e2e61
Joel Thiessen <jatjat@users.noreply.github.com>
2016-07-03 19:32:26 -0500

    ...and there was much rejoicing
    FastSLAM works (both landmark additions + updates)

--- src/main/kotlin/ca/joelathiessen/kaly2/odometry/CarModel.kt
+++ src/main/kotlin/ca/joelathiessen/kaly2/odometry/CarModel.kt
@@ -5,7 +5,7 @@ import lejos.robotics.navigation.Pose
 import java.util.*
 
 class CarModel : MotionModel {
-    private val ANGLE_ERROR = 0.005
+    private val ANGLE_ERROR = 0.0005
     private val DIST_ERROR = 0.5
     private val random = Random()
 
@@ -14,12 +14,14 @@ class CarModel : MotionModel {
         val robotMoveAngle = Math.toRadians(startReadPose.angleTo(endReadPose.location).toDouble())
         val dMoveAngStartPoseHead = robotMoveAngle - startReadPose.heading
 
-        val moveAngle = (inputPose.heading + dMoveAngStartPoseHead) //+ random.nextGaussian() * ANGLE_ERROR
+        val angleAdjust = random.nextGaussian() * ANGLE_ERROR
+
+        val moveAngle = (inputPose.heading + dMoveAngStartPoseHead) //+ angleAdjust
         val moveDist = startReadPose.location.distance(endReadPose.location) + random.nextGaussian() * DIST_ERROR
 
         val dX = Math.cos(moveAngle.toDouble()) * moveDist
         val dY = Math.sin(moveAngle.toDouble()) * moveDist
-        val dHeading = endReadPose.heading - startReadPose.heading + random.nextGaussian() * ANGLE_ERROR
+        val dHeading = endReadPose.heading - startReadPose.heading //+ angleAdjust
 
         val movedPose = Pose((inputPose.x + dX).toFloat(), (inputPose.y + dY).toFloat(), (inputPose.heading + dHeading).toFloat())
         return movedPose
