7c8fa2378e5dd914cedf3fe32588d56afcabe1d4
Joel Thiessen <jatjat@users.noreply.github.com>
2016-07-06 22:29:23 -0500

    Cleanup drawing code

--- src/test/kotlin/ca/joelathiessen/kaly2/tests/pc/acceptance/FastSLAMView.kt
+++ src/test/kotlin/ca/joelathiessen/kaly2/tests/pc/acceptance/FastSLAMView.kt
@@ -7,17 +7,14 @@ import ca.joelathiessen.kaly2.slam.FastUnbiasedResampler
 import ca.joelathiessen.kaly2.slam.NNDataAssociator
 import ca.joelathiessen.kaly2.subconscious.sensor.SensorInfo
 import ca.joelathiessen.kaly2.tests.pc.unit.slam.getFeatureForPosition
-import lejos.robotics.navigation.Pose
 import org.mockito.Mockito
 import java.awt.Color
 import java.awt.Graphics
 import java.util.*
 import javax.swing.JFrame
-import javax.swing.JLabel
 import javax.swing.JPanel
 import javax.swing.WindowConstants
 import kotlin.concurrent.fixedRateTimer
-import kotlin.concurrent.thread
 
 object FastSLAMDemo {
     @JvmStatic fun main(args: Array<String>) {
@@ -33,7 +30,8 @@ object FastSLAMDemo {
 class FastSLAMView : JPanel() {
     val ROT_RATE = 0.03
     val STEP_DIST = 2
-    val STEP_STD_DEV = 0.5
+    val STEP_ROT_STD_DEV = 0.01
+    val STEP_DIST_STD_DEV = 0.5
     val MIN_WIDTH = 400.0
 
     val SENSOR_STD_DEV = 0.1
@@ -60,6 +58,10 @@ class FastSLAMView : JPanel() {
     var theta = 0.1
     var times = 0
 
+    var drawOdoLocs: List<Pair<Int, Int>> = ArrayList()
+    var drawParticlePoses: List<Pair<Int, Int>> = ArrayList()
+    val drawRealObjectLocs = realObjectLocs.map { Pair(it.x.toInt(), it.y.toInt()) }
+
     init {
         this.setSize(MIN_WIDTH.toInt(), MIN_WIDTH.toInt())
 
@@ -74,40 +76,43 @@ class FastSLAMView : JPanel() {
 
     fun mainLoop() {
         //move the robot
-        theta += ROT_RATE
-        x += Math.cos(theta) * STEP_DIST + STEP_STD_DEV * random.nextGaussian()
-        y += Math.sin(theta) * STEP_DIST + STEP_STD_DEV * random.nextGaussian()
+        theta += ROT_RATE + STEP_ROT_STD_DEV * random.nextGaussian()
+        x += Math.cos(theta) * STEP_DIST + STEP_DIST_STD_DEV * random.nextGaussian()
+        y += Math.sin(theta) * STEP_DIST + STEP_DIST_STD_DEV * random.nextGaussian()
         realPos = RobotPose(times, 0f, x.toFloat(), y.toFloat(), theta.toFloat())
 
         //make features as the robot sees them
         val features = realObjectLocs.map { getFeatureForPosition(x, y, theta, it.x, it.y, SENSOR_STD_DEV) }
-        odoLocs.add(realPos)
         synchronized(drawLock) {
+            odoLocs.add(realPos)
             slam.addTimeStep(features, realPos)
         }
-       // this@FastSLAMView.invalidate()
         this@FastSLAMView.repaint()
     }
 
-    override  fun paint(graphics: Graphics) {
+
+    override fun paint(graphics: Graphics) {
+
+        synchronized(drawLock) {
+            drawOdoLocs = odoLocs.map { Pair(it.x.toInt(), it.y.toInt()) }
+            drawParticlePoses = slam.particlePoses.map { Pair(it.x.toInt(), it.y.toInt()) }
+        }
+
         // track the robot's real/odometric position
         graphics.color = Color.RED
-        synchronized(drawLock) {
-            odoLocs.forEach { graphics.drawRect(it.x.toInt(), it.y.toInt(), 2, 2) }
-        }
+        drawOdoLocs.forEach { graphics.drawRect(it.first, it.second, 2, 2) }
 
         // draw the particles
         graphics.color = Color.blue
-        synchronized(drawLock) {
-            slam.particlePoses.forEach {
-                graphics.drawRect(it.x.toInt(), it.y.toInt(), 2, 2)
-            }
+        drawParticlePoses.forEach {
+            graphics.drawRect(it.first, it.second, 2, 2)
         }
 
         // draw the objects
         graphics.color = Color.GREEN
-            realObjectLocs.forEach {
-                graphics.drawRect(it.x.toInt(), it.y.toInt(), 2, 2)
-            }
+        drawRealObjectLocs.forEach {
+            graphics.drawRect(it.first, it.second, 2, 2)
+        }
+
     }
 }
