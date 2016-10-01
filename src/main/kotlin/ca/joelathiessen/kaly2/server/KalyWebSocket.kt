b5f46d2dc678c719c788388053401f492753bc25
Joel Thiessen <jatjat@users.noreply.github.com>
2016-10-01 16:08:50 -0500

    Try to avoid harmless server error messages
    Server error messages are printed after hard websocket disconnects. The
    robot simulations still end properly. Not all of these messages are
    prevented--this commit may be a placebo.

--- src/main/kotlin/ca/joelathiessen/kaly2/server/KalyWebSocket.kt
+++ src/main/kotlin/ca/joelathiessen/kaly2/server/KalyWebSocket.kt
@@ -10,6 +10,8 @@ class KalyWebSocket(private val robotsManager: RobotsManager, private val rid: L
     private lateinit var robotHandler: RobotHandler
     private val handleRTMessageCaller = { sender: Any, msg: RTMsg -> HandleRTMessage(sender, msg) }
     private val gson = GsonBuilder().create()!!
+    private var closed = false
+    private var closedLock = Any()
 
     override fun onOpen(connection: WebSocket.Connection) {
         this.connection = connection
@@ -27,12 +29,20 @@ class KalyWebSocket(private val robotsManager: RobotsManager, private val rid: L
         if(robotHandler.rtUpdateEvent.length == 0) {
             robotsManager.removeHandler(rid)
         }
+        synchronized(closedLock) {
+            closed = true;
+        }
     }
 
-    // Unlike Jetty 9, as far as I'm aware, Jetty 7 does not support asynchronous message sending
+    // Unlike Jetty 9, Jetty 7 does not support asynchronous message sending
+    // See http://jetty.4.x6.nabble.com/jetty-dev-WebSocket-Async-Read-Write-td4466406.html
     fun HandleRTMessage(sender: Any, message: RTMsg) {
         connectionExecutor.execute {
-            connection.sendMessage(gson.toJson(message))
+            synchronized(closedLock) {
+                if (!closed) {
+                    connection.sendMessage(gson.toJson(message))
+                }
+            }
         }
     }
 }
