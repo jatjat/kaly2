5e78e02539e914a1069bd899e5d590fccf8bf845
Joel Thiessen <jatjat@users.noreply.github.com>
2017-02-22 20:17:12 -0600

    Abstract Float-Double casts in CarModel

--- src/main/kotlin/ca/joelathiessen/kaly2/odometry/CarModel.kt
+++ src/main/kotlin/ca/joelathiessen/kaly2/odometry/CarModel.kt
@@ -1,28 +1,28 @@
 package ca.joelathiessen.kaly2.odometry
 
 import lejos.robotics.navigation.Pose
-import java.util.*
+import ca.joelathiessen.util.*
 
 class CarModel : MotionModel {
-    private val ANGLE_ERROR = 0.01
-    private val DIST_ERROR = 0.5
-    private val random = Random()
+    private val ANGLE_ERROR = 0.01f
+    private val DIST_ERROR = 0.5f
+    private val random = FloatRandom(0)
 
     override fun moveRandom(inputPose: Pose, startReadPose: RobotPose, endReadPose: RobotPose): Pose {
 
-        val robotMoveAngle = Math.toRadians(startReadPose.angleTo(endReadPose.location).toDouble())
+        val robotMoveAngle = FloatMath.toRadians(startReadPose.angleTo(endReadPose.location))
         val dMoveAngStartPoseHead = robotMoveAngle - startReadPose.heading
 
         val angleAdjust = random.nextGaussian() * ANGLE_ERROR
 
         val moveAngle = (inputPose.heading + dMoveAngStartPoseHead) + angleAdjust
-        val moveDist = startReadPose.location.distance(endReadPose.location) + (random.nextGaussian() * DIST_ERROR)
+        val moveDist = startReadPose.distanceTo(endReadPose.location) + (random.nextGaussian() * DIST_ERROR)
 
-        val dX = Math.cos(moveAngle.toDouble()) * moveDist
-        val dY = Math.sin(moveAngle.toDouble()) * moveDist
+        val dX = FloatMath.cos(moveAngle) * moveDist
+        val dY = FloatMath.sin(moveAngle) * moveDist
         val dHeading = endReadPose.heading - startReadPose.heading + angleAdjust
 
-        val movedPose = Pose((inputPose.x + dX).toFloat(), (inputPose.y + dY).toFloat(), (inputPose.heading + dHeading).toFloat())
+        val movedPose = Pose((inputPose.x + dX), (inputPose.y + dY), (inputPose.heading + dHeading))
         return movedPose
     }
 
